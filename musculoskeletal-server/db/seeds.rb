4d23148dcec371aa4688c221f70e03845c56e0e7
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-02-03 16:43:19 -0500

    added seed data for bodypart and region

--- musculoskeletal-server/db/seeds.rb
+++ musculoskeletal-server/db/seeds.rb
@@ -1,5 +1,39 @@
 puts "🌱 Seeding messages..."
 
+Bodypart.destroy_all
+Region.destroy_all
 
+bodyparts = [
+    {name: "Shoulder and Rotator Cuff"},
+    {name: "Chest"},
+    {name: "Back"},
+    {name: "Arms"},
+    {name: "Abdominals"},
+    {name: "Glutes"},
+    {name: "Quadriceps"},
+    {name: "Hamstrings"},
+    {name: "Miscellaneous"}
+]
+
+bodyparts.each do |bodypart|
+    Bodypart.create(name: bodypart[:name])
+end
+
+puts 'bodyparts uploaded'
+
+regions = [
+    {name: "Skull"},
+    {name: "Spine"},
+    {name: "Thorax"},
+    {name: "Pelvis"},
+    {name: "Upper Extremity"},
+    {name: "Lower Extremity"}
+]
+
+regions.each do |region|
+    Region.create(name: region[:name])
+end
+
+puts 'regions uploaded'
 
 puts "✅ Done seeding!"
