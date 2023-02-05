e2e292b5f81359ed58198f0f46d748830db45516
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-02-05 00:18:42 -0500

    styled tables using CSS

--- musculoskeletal-client/src/BonesTable.js
+++ musculoskeletal-client/src/BonesTable.js
@@ -9,12 +9,23 @@ function BonesTable(){
         .then(bones => setBones(bones));
       }, []);
 
+    const tableHeader = (
+        <tr className='header'>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Image link</td>
+        </tr> 
+        )
+
     const [bones, setBones] = useState([]);
     const boneTableRows = bones.map(bone => <BoneTableRow key={bone.id} bone={bone} />)
     return (
         <div>
             <h2>Bones of the Skeleton</h2>
-            {boneTableRows}
+            <table>
+                {tableHeader}
+                {boneTableRows}
+            </table>
         </div>
     )
 }
--- musculoskeletal-client/src/MusclesTable.js
+++ musculoskeletal-client/src/MusclesTable.js
@@ -9,13 +9,28 @@ function MusclesTable(){
         .then(muscles => setMuscles(muscles));
       }, []);
 
+    const tableHeader = (
+        <tr className='header'>
+            <td>Name</td>
+            <td>Origin</td>
+            <td>Insertion</td>
+            <td>Action</td>
+            <td>Innervation</td>
+            <td>Blood Supply</td>
+            <td>Image Link</td>
+        </tr> 
+        )
+
     const [muscles, setMuscles] = useState([]);
     const muscleTableRows = muscles.map(muscle => <MuscleTableRow key={muscle.id} muscle={muscle}/>);
 
     return(
         <div>
             <h2>Muscles involved in exercise</h2>
-            {muscleTableRows}
+            <table>
+                {tableHeader}
+                {muscleTableRows}
+            </table>
         </div>
     )
 }
